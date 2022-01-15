04722f77ec3c68eeca7f1c52b7fc29237f0676e3
omar_xs2 <omar.soliman306@gmail.com>
2022-01-15 14:48:52 +0200

    first commit

---
+++ src/components/Aboutus.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {
+    Breadcrumb, BreadcrumbItem, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, CardHeader, Media
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger, Loop } from 'react-animation-components';
+
+function About(props) {
+
+    const RenderLeader = ({ leader }) => {
+        return (
+            <Stagger in chunk={4}>
+                <Fade >
+                    <Media className='row' style={{ marginBottom: '30px' }}>
+                        <div className='col-3' style={{ padding: '20px', width: '150px' }}>
+                            <Media left top href="">
+                                <Media
+                                    object
+                                    src={leader.image}
+                                    alt="image"
+                                />
+                            </Media>
+                        </div>
+                        <div className='col-9'>
+                            <Media body>
+                                <Media heading>{leader.name}</Media>
+                                {leader.designation ? <h2>{leader.designation}</h2> : null}
+                                {leader.description}
+                            </Media>
+                        </div>
+                    </Media>
+                </Fade>
+            </Stagger>
+        );
+    }
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>About Us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>About Us</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row row-content">
+                <div className="col-12 col-md-6">
+                    <h2>Our History</h2>
+                    <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
+                    <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
+                </div>
+                <div className="col-12 col-md-5">
+                    <Card>
+                        <CardHeader className="bg-primary text-white">Facts At a Glance</CardHeader>
+                        <CardBody>
+                            <dl className="row p-1">
+                                <dt className="col-6">Started</dt>
+                                <dd className="col-6">3 Feb. 2013</dd>
+                                <dt className="col-6">Major Stake Holder</dt>
+                                <dd className="col-6">HK Fine Foods Inc.</dd>
+                                <dt className="col-6">Last Year's Turnover</dt>
+                                <dd className="col-6">$1,250,375</dd>
+                                <dt className="col-6">Employees</dt>
+                                <dd className="col-6">40</dd>
+                            </dl>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12">
+                    <Card>
+                        <CardBody className="bg-faded">
+                            <blockquote className="blockquote">
+                                <p className="mb-0">You better cut the pizza in four pieces because
+                                    I'm not hungry enough to eat six.</p>
+                                <footer className="blockquote-footer">Yogi Berra,
+                                    <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                                        P. Pepe, Diversion Books, 2014</cite>
+                                </footer>
+                            </blockquote>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+            <div className="row row-content">
+                <div className="col-12">
+                    <h2>Corporate Leadership</h2>
+                </div>
+                <div className="col-12">
+                    <Media list>
+                        {
+                            props.leaders.leaders.map(leader => {
+                                return <RenderLeader leader={leader} />
+                            })
+                        }
+                    </Media>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem,
+    Button, Modal, ModalHeader, ModalBody,
+    Row, Col, Label, Nav, NavItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+const RenderDish = ({ dish }) => {
+    if (dish != null) {
+        return (
+            <div className=" col-12 col-md-5 m-1">
+                <FadeTransform
+                    in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card key={dish.id}>
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
+            </div>
+
+        );
+    } else {
+        return <div></div>;
+    }
+}
+
+function RenderComments({ comments, postComment, dishId }) {
+    if (comments != null) {
+        return (
+            <div key={comments.id} className="col-12 col-md-12">
+                <h4>Comments:</h4>
+                <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            </Fade>
+                        );
+                    })}
+                </Stagger>
+                <CommentForm dishId={dishId} postComment={postComment} />
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
+
+}
+
+const DishDetail = (props) => {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-5">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-7">
+                        <RenderComments comments={props.comments}
+                            postComment={props.postComment}
+                            dishId={props.dish.id}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
+
+}
+
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.state = {
+            isModalOpen: false
+        };
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+
+    render() {
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        const isNumber = (val) => !isNaN(Number(val));
+        return (
+            <div>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button outline onClick={this.toggleModal}><span className="fa fa-pen-in fa-lg"></span>Submit Comment</Button>
+                    </NavItem>
+                </Nav>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={12}>Contact </Label>
+                                <Col md={12}>
+                                    <Control.text type="number" model=".rating" id="rating" name="rating"
+                                        placeholder="Rating"
+                                        className="form-control"
+                                        validators={{
+                                            required, isNumber
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".rating"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="12"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{ size: 12, offset: 0 }}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+
+
+export default DishDetail;
